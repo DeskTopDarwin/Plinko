3a074d51c906a14221d84e3195d87dca1cdc3c54
DeskTopDarwin <93140023+DeskTopDarwin@users.noreply.github.com>
2021-11-01 14:20:18 -0400

    added win lose
    added trigger event

--- Assets/Scripts/BallMovement.cs
+++ Assets/Scripts/BallMovement.cs
@@ -5,12 +5,20 @@ using UnityEngine;
 public class BallMovement : MonoBehaviour
 {
     Rigidbody rg;
-
+    public float offset;
+    public float speed;
     public Transform leftWall;
     public Transform rightWall;
+
+   
+    private Transform initialTransform;
+    private bool isGameOver = false;
+
     private void Awake()
     {
         rg = gameObject.GetComponent<Rigidbody>();
+        initialTransform = transform;
+
     }
     // Start is called before the first frame update
     void Start()
@@ -21,22 +29,43 @@ public class BallMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("t: " + transform.position.x);
-        Debug.Log("lw: " + leftWall.position.x);
-        if (Input.GetKey(KeyCode.LeftArrow) && rg.isKinematic && transform.position.x < leftWall.position.x)
+        //Debug.Log("t: " + transform.position.x);
+        //Debug.Log("lw: " + leftWall.position.x);
+        if (Input.GetKey(KeyCode.LeftArrow) && rg.isKinematic && transform.position.x < leftWall.position.x - offset)
         {
-            transform.position = transform.position + new Vector3(1 * Time.deltaTime, 0, 0);
+            transform.position = transform.position + new Vector3(speed * Time.deltaTime, 0, 0);
         }
-        else if (Input.GetKey(KeyCode.RightArrow) && rg.isKinematic && transform.position.x > rightWall.position.x)
+        else if (Input.GetKey(KeyCode.RightArrow) && rg.isKinematic && transform.position.x > rightWall.position.x + offset)
         {
-            transform.position = transform.position + new Vector3(-1 * Time.deltaTime, 0, 0);
+            transform.position = transform.position + new Vector3(-speed * Time.deltaTime, 0, 0);
         }
         
         else if (Input.GetKey(KeyCode.Space))
         {
             rg.isKinematic = false;
         }
-        
-        //transform.position = transform.position + new Vector3(1, 0, 0);
+
+        else if (Input.anyKeyDown && isGameOver)
+        {
+            transform.position = initialTransform.position;
+            isGameOver = false;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        string win = "win";
+        string lose = "lose";
+
+        if (other.CompareTag(win))
+        {
+            Debug.Log("win");
+            isGameOver = true;
+        }
+        else if(other.CompareTag(lose))
+        {
+            Debug.Log("lose");
+            isGameOver = true;
+        }
     }
 }
